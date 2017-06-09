248c918e77cc9da0754cf0c02a925eb8350d9e7c
Gen Tada <gen.e.manipulation@gmail.com>
2017-06-09 17:56:56 +0900

    Angular2によるモダンWeb開発 テキスト p.103 まで

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'こんにちは！';
+	key = '数値の5倍';
+	value = 0;
+	dataArray = [
+		'1a', '2b', '3c', '4d', '5e', '6f', '7g', '8h', '9i', '10j',
+		'11k', '12l', '13m', '14n', '15o', '16p', '17q', '18r', '19s', '20t',
+		'21u', '22v', '23w', '24x', '25y', '26z'
+	];
 }
